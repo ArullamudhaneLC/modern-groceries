4780990dff96348f6a956b7591ed92f5574f4ea4
Arullamudhane Loganathan Chithra <larullamudhane@athenahealth.com>
2021-12-31 00:21:03 +0530

    first commit

---
+++ src/main/java/com/example/moderngroceries/ModernGroceriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.moderngroceries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernGroceriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernGroceriesApplication.class, args);
+	}
+
+}
