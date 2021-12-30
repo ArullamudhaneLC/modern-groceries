41bde6f1085a2713ab3d0c95a366d5e29d573288
Arullamudhane Loganathan Chithra <larullamudhane@athenahealth.com>
2021-12-31 00:36:25 +0530

    created login related files for testing

---
+++ src/main/java/com/example/moderngroceries/controller/LogInController.java
@@ -0,0 +1,9 @@
+package com.example.moderngroceries.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogInController {
+
+
+}
---
+++ src/main/java/com/example/moderngroceries/service/LogInServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.moderngroceries.service;
+
+public class LogInServiceImpl {
+}
